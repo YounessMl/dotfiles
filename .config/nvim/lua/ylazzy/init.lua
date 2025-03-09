4a98683ab888fcc4c0f9a4146b50ada2ffa37341
YounessMl <younessmelloukidev@gmail.com>
2025-03-09 00:28:12 +0000

    remove leftovers

--- .config/nvim/lua/ylazzy/init.lua
+++
@@ -1,2 +0,0 @@
-require("ylazzy.remaps")
-require("ylazzy.options")
--- .config/nvim/lua/ylazzy/remaps.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader=" "
-vim.keymap.set("n", "<leader>pv", vim.cmd.E)
-
--- Moving lines in Visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
